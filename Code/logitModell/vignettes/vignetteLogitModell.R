09762668c735b9aac6cae11fcf6f383f6d38e8b7
lxs <XuanSon@Xuans-MacBook-Pro.local>
2018-03-31 17:44:32 +0200

    .

--- Code/logitModell/vignettes/vignetteLogitModell.R
+++ Code/logitModell/vignettes/vignetteLogitModell.R
@@ -8,7 +8,7 @@ library(logitModell)
 testData <- read.csv("https://stats.idre.ucla.edu/stat/data/binary.csv")
 testData <- testData[1:100,]
 testData$rank <- factor(testData$rank)
-testModell <- as.formula("admit ~ gre + gpa + rank")
+testFormula <- as.formula("admit ~ gre + gpa + rank")
 testModelFrame <- model.frame(admit ~ gre + gpa + rank, testData)
 
 # Alternatively one can use the following code to test the result on another data set. This data set is default built in R.
@@ -16,14 +16,24 @@ testModelFrame <- model.frame(admit ~ gre + gpa + rank, testData)
 # data(cats, package = "MASS")
 # testData <- cats[1:100,]
 # #testData$Sex <- factor(testData$Sex, labels = c(0,1))
-# testModell <- as.formula("Sex ~ Bwt + Hwt")
+# testFormula <- as.formula("Sex ~ Bwt + Hwt")
 # testModelFrame <- model.frame(Sex ~ Bwt + Hwt, testData)
 
-logitMod(formula = testModell, data = testData)
-glm(formula = testModell, data = testData, family = "binomial")
-
 ## ----Fit logit model-----------------------------------------------------
-fittedLogModell <- logitMod(formula = testModell, data = testData)
+fittedLogModell <- logitMod(formula = testFormula, data = testData)
+standardModell <- glm(formula = testFormula, data = testData, family = "binomial")
+
+all(all.equal(standardModell$coefficients,
+              as.numeric(fittedLogModell$coefficients),
+              check.attributes = FALSE),
+    all.equal(vcov(standardModell), 
+              fittedLogModell$vcov,
+              check.attributes = FALSE, tolerance = exp(-10)),
+    all.equal(residuals(standardModell),
+              as.numeric(fittedLogModell$devianceResidual),
+              check.attributes = FALSE, tolerance = exp(-10)),
+    all.equal(standardModell$df.residual, fittedLogModell$dfRes),
+    all.equal(standardModell$df.null, fittedLogModell$dfNull))
 
 ## ----Print method--------------------------------------------------------
 print(fittedLogModell)
@@ -33,4 +43,5 @@ summary(fittedLogModell)
 
 ## ----Plot method---------------------------------------------------------
 plot(fittedLogModell)
+plot(glm(formula = testFormula, data = testData, family = "binomial"))
 
